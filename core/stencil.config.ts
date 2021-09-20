fe5ec4cac98993aae4972c3f1030381bc9f82a49
Rafael Melo <rsmelo_@hotmail.com>
2021-09-20 12:24:04 -0300

    Ser core and react

---
+++ core/stencil.config.ts
@@ -0,0 +1,17 @@
+import { Config } from '@stencil/core';
+import { reactOutputTarget } from '@stencil/react-output-target';
+
+export const config: Config = {
+  namespace: 'core',
+  outputTargets: [
+    reactOutputTarget({
+      proxiesFile: '../react/src/components.ts',
+      includeDefineCustomElements: true,
+      loaderDir: '../../core/loader'
+    }),
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+  ],
+};
---
+++ react/src/components.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from '../../core/dist/types/components';
+
+import { defineCustomElements } from '../../core/loader';
+
+defineCustomElements();
+export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
